f8d11ea64801cb458f98f5f468cd002aa192927b
unclewanya <Vv9R$ftG0iwG>
2018-10-07 20:48:33 +0200

    XX

--- app/src/main/java/hu/uwsoft/unclewanya/uwdashboard/MainActivity.java
+++ app/src/main/java/hu/uwsoft/unclewanya/uwdashboard/MainActivity.java
@@ -1,25 +1,65 @@
 package hu.uwsoft.unclewanya.uwdashboard;
 
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+    Button btnStart;
+    Button btnExit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_main);
-        Button btnStart = (Button) findViewById(R.id.btnStart);
+
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        // this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        final Handler handler = new Handler();
+        Timer timer = new Timer(false);
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Do whatever you want
+                        TextView tvText = (TextView) findViewById(R.id.tvText);
+                        tvText.setText(Calendar.getInstance().getTime().toString());
+                    }
+                });
+            }
+        };
+
+        timer.schedule(timerTask, 1000); // 1000 = 1 second.
+        btnStart = (Button) findViewById(R.id.btnStart);
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+            TextView tvText = (TextView) findViewById(R.id.tvText);
+            tvText.setTextColor(Color.BLUE);
+            }
+        });
+
+        btnExit = (Button) findViewById(R.id.btnExit);
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 finish();
-//                TextView tvText = (TextView) findViewById(R.id.tvText);
-//                tvText.setTextColor(red);
             }
         });
     }
