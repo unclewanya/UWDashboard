0d8a5007211b7b886955fa405da080eadfd588eb
unclewanya <Vv9R$ftG0iwG>
2018-10-07 18:38:40 +0200

    XX

--- app/src/main/java/hu/uwsoft/unclewanya/uwdashboard/MainActivity.java
+++ app/src/main/java/hu/uwsoft/unclewanya/uwdashboard/MainActivity.java
@@ -2,6 +2,9 @@ package hu.uwsoft.unclewanya.uwdashboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        Button btnStart = (Button) findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+//                TextView tvText = (TextView) findViewById(R.id.tvText);
+//                tvText.setTextColor(red);
+            }
+        });
     }
+
+
 }
